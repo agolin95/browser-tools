fbc2ab6c60513f5080366ddaa4efd9b4c5d4dc46
Alexander Golin <alexander@Alexanders-MacBook-Pro.local>
2021-03-09 18:19:59 -0500

    auto expand issue links working for sidebar

--- tools.js
+++ tools.js
@@ -1,5 +1,15 @@
 $(function() {
 	setTimeout(() => {  
 		$('#show-more-links-link').click()
-	}, 1000);
+	}, 5000);
+	
+    var firstUrl = window.location.href
+	document.addEventListener('click', function() {
+		var newUrl = window.location.href;
+		if (firstUrl != newUrl) {
+			setTimeout(() => {  
+				$('#show-more-links-link').click()
+			}, 1000);
+		}
+	});
 });
