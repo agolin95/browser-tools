1d170e6360ee00eabbc0cde1a5673d60faef49f4
Alexander Golin <agolin95@gmail.com>
2023-03-02 17:36:55 -0500

    calendar

---
+++ userscripts/google/calendar.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Calendar Cleanup
+// @namespace    https://alexandergolin.com/
+// @version      0.1
+// @description  Visual Tweaks for Google Calendar
+// @author       Alexander Golin
+// @match        https://calendar.google.com/*
+// @icon         https://alexandergolin.com/img/logo.png
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/agolin95/browser-tools/main/userscripts/google/calendar.js
+// @updateURL    https://raw.githubusercontent.com/agolin95/browser-tools/main/userscripts/google/calendar.js
+// @require https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
+
+var $ = window.jQuery;
+
+(function () {
+    'use strict';
+    console.log("Discord Style Cleanup Running")
+    var styleSheet = document.createElement("style");
+    styleSheet.innerText = getStyles();
+    document.body.appendChild(styleSheet);
+
+})();
+
+function getStyles() {
+    return `
+		:root
+		{
+        --black: black;
+		--custom-highlight: red;
+		--custom-buffer: rgba(255, 0, 0, 0.3);
+		}
+
+        body,
+        header#gb,
+        .v83gc,
+        .J2aUD.T8M5bd .sx5BGe::before,
+        .SGFqbe.T8M5bd .sx5BGe::before,
+        .W0m3G::before,
+        .W0m3G:hover .r4nke,
+        .W0m3G .r4nke,
+        .Gk2izd,
+        .GENA3c,
+        .ULpymb,
+        .eADW5d,
+        .buGMKc {
+            background: var(--black)!important;
+        }
+
+        .J2aUD.T8M5bd .qmFsL::after,
+        .SGFqbe.T8M5bd .qmFsL::after,
+        .hEtGGf::after,
+        img.gb_Fc {
+            display: none;
+        }
+
+        .HDIIVe,
+        .eh5oYe .hnGhZ{
+            scrollbar-width: none;
+        }
+
+	`;
+}
